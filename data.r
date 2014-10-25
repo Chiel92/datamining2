293db5254003d90c6785eb744902e4686a47acf9
Chiel92 <ctje92@gmail.com>
2014-10-25 20:17:14 +0200

    bronkerbosch working

--- data.r
+++ data.r
@@ -1,3 +1,11 @@
-coronary.dat <- read.table("coronary.txt",header=T)
+coronary.dat <- read.table('coronary.txt', header=T)
 
 observed <- table(coronary.dat)
+
+graph <- matrix(c(0, 1, 0, 0, 0, 0,
+                  1, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0), 6)
+
--- functions.r
+++ functions.r
@@ -1,12 +1,47 @@
+# Utils
+assert = function(bool) if (!bool) stop('Assertion error')
 
-# graph.init
+graph.empty = function(size)
+{
+    return(matrix(replicate(size * size, 0), size))
+}
 
 # graph.search
-graph.search <- function(observed, graph)
+graph.search = function(observed, graph)
 {
     # Detect the cliques from the graph
 
     # Use loglin to generate fitted model to the cliques
 
     # Construct neighbourhood and advance
+}
+
+bronkerbosch = function(graph)
+{
+    recursion <- function(include, rest, exclude)
+    {
+        assert(length(include) > 0 | length(rest) > 0 | length(exclude) > 0)
+        assert(length(intersect(include, rest)) == 0)
+        assert(length(intersect(include, exclude)) == 0)
+        assert(length(intersect(rest, exclude)) == 0)
+
+        if (length(exclude) == 0 & length(rest) == 0)
+            return(list(include))
+
+        result <- c()
+        for (v in rest)
+        {
+            neighbors <- which(graph[v,] == 1)
+            recursive_result <- recursion(union(include, v),
+                                          intersect(rest, neighbors),
+                                          intersect(exclude, neighbors))
+            rest <- setdiff(rest, v)
+            exclude <- union(exclude, v)
+
+            result <- c(result, recursive_result)
+        }
+        return(result)
+    }
+
+    return(recursion(c(), 1:nrow(graph), c()))
 }
