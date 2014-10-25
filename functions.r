382c92db1ed61460ee19420c50be6ad9d755dd07
Chiel92 <ctje92@gmail.com>
2014-10-25 21:04:02 +0200

    Implement local search; not tested

--- functions.r
+++ functions.r
@@ -9,12 +9,57 @@ graph.empty = function(size)
 # graph.search
 graph.search = function(observed, graph)
 {
+    score <- graph.assess(observed, graph)
+
+    best_neighbor <- NULL
+    best_neighbor_score <- 0
+
+    while(TRUE)
+    {
+        # Construct neighborhood
+        for (v in 2:nrow(graph))
+        {
+            for (w in v:nrow(graph))
+            {
+                # Transform graph into a neighbor
+                graph[v,w] <- 1 - graph[v,w]
+                graph[w,v] <- 1 - graph[w,v]
+
+                # Evaluate the neighbor
+                neighbor_score <- graph.assess(graph)
+                if (neighbor_score > score)
+                {
+                    best_neighbor <- graph
+                    best_neighbor_score <- neighbor_score
+                }
+
+                # Undo the transformation
+                graph[v,w] <- 1 - graph[v,w]
+                graph[w,v] <- 1 - graph[w,v]
+            }
+        }
+
+        # Return if no improvement else continue with best neighbors
+        if (!is.null(best_neighbor))
+        {
+            graph <- best_neighbor
+            score <- best_neighbor_score
+        }
+        else
+            return(graph)
+    }
+
+}
+
+graph.assess = function(observed, graph)
+{
     # Detect the cliques from the graph
     cliques <- bronkerbosch(graph)
 
     # Use loglin to generate fitted model to the cliques
+    model <- loglin(observed, cliques)
 
-    # Construct neighbourhood and advance
+    # Compute score and return
 }
 
 bronkerbosch = function(graph)
