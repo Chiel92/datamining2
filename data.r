aa42a46a142ee29550a67e74f768c915ddf13580
Chiel92 <ctje92@gmail.com>
2014-12-08 09:24:26 +0100

    extra data

--- data.r
+++ data.r
@@ -1,6 +1,6 @@
 coronary.dat <- read.table('coronary.txt', header=T)
 rhc.dat <- read.csv('rhc-small.txt', header=T)
-rhc_small.dat <- rhc.dat[sample(1:dim(rhc.dat)[1], 1000),]
+#rhc_small.dat <- rhc.dat[sample(1:dim(rhc.dat)[1], 1000),]
 
 observed <- table(rhc.dat)
 
